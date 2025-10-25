44702cc84e781375abaa985c7b735277f4a6806c
Viktor Zepeda sanchez <viktor_zepeda_sanchez@sfu.ca>
2025-10-25 03:40:57 -0700

    match output

--- cmpt361_a3/a3.js
+++ cmpt361_a3/a3.js
@@ -43,25 +43,21 @@ Rasterizer.prototype.drawTriangle = function(v1, v2, v3) {
   const [x2, y2] = v2;
   const [x3, y3] = v3;
 
-  xmin = Math.ceil(Math.min(x1,x2,x3));
-  xmax = Math.ceil(Math.max(x1,x2,x3));
-  ymin = Math.ceil(Math.min(y1,y2,y3));
-  ymax = Math.ceil(Math.max(y1,y2,y3));
+  const xmin = Math.ceil(Math.min(x1,x2,x3));
+  const xmax = Math.ceil(Math.max(x1,x2,x3));
+  const ymin = Math.ceil(Math.min(y1,y2,y3));
+  const ymax = Math.ceil(Math.max(y1,y2,y3));
 
   for (let x = xmin; x <= xmax; x++) {
     for (let y = ymin; y <= ymax; y++) {
       let p = [x + 0.5, y + 0.5];
-      const [u, v, w, color] = barycentricCoordinates(v1, v2, v3, p);
-      
-      if (u >= 0 && v >= 0 && w >= 0) {
+      let color = barycentricCoordinates(v1, v2, v3, p);
+      if (pointIsInsideTriangle(v1, v2, v3, p)) {
         this.setPixel(x, y, color);
       }
     }
   }
   // TODO/HINT: use this.setPixel(x, y, color) in this function to draw triangle
-  // this.setPixel(Math.floor(x1), Math.floor(y1), [r1, g1, b1]);
-  // this.setPixel(Math.floor(x2), Math.floor(y2), [r2, g2, b2]);
-  // this.setPixel(Math.floor(x3), Math.floor(y3), [r3, g3, b3]);
 }
 
 
@@ -88,9 +84,9 @@ const DEF_INPUT = [
 function color_interp(color1,color2,frac) {
   const [r1,g1,b1] = color1;
   const [r2,g2,b2] = color2;
-  let r = (r2 - r1) * frac + r1;
-  let g = (g2 - g1) * frac + g1;
-  let b = (b2 - b1) * frac + b1;
+  const r = (r2 - r1) * frac + r1;
+  const g = (g2 - g1) * frac + g1;
+  const b = (b2 - b1) * frac + b1;
   return [r,g,b]
 }
 
@@ -99,22 +95,34 @@ function barycentricCoordinates(v1, v2, v3, p) {
   const [x1, y1, [r1,g1,b1]] = v1;
   const [x2, y2, [r2,g2,b2]] = v2;
   const [x3, y3, [r3,g3,b3]] = v3;
-  const [px,py] = p;
-  // vertex colors
-  const v1c = [r1,g1,b1];
-  const v2c = [r2,g2,b2];
-  const v3c = [r3,g3,b3];
+  const [px, py] = p;
   // calculate the smaller areas defined by vertices and p
-  const a1 = (x2 - x1) * (py - y1) - (y2 -y1)*(px -x1); // v1 p v2
-  const a2 = (x3 - x2) * (py - y2) - (y3 -y2)*(px -x2); // v2 p v3
-  const a3 = (x3 - x1) * (py - y1) - (y3 -y1)*(px -x1); // v1 p v3
-  const A = a1 + a2 + a3;
-  // barycentric coordinates
+
+  //const edge = (x0, y0, x1, y1, px, py) => (px - x0) * (y1 - y0) - (py - y0) * (x1 - x0);
+  
+  const a1 = (x2 - x1) * (py - y1) - (y2 - y1) * (px - x1);
+  const a2 = (x3 - x2) * (py - y2) - (y3 - y2) * (px - x2);
+  const a3 = (x1 - x3) * (py - y3) - (y1 - y3) * (px - x3);
+
+
+  const A = (y2 - y3)*(x1 - x3) + (x3 - x2)*(y1 - y3);
+  //barycentric coordinates
   const u = a1 / A;
   const v = a2 / A;
   const w = 1 - u - v;
+  // const area = edge(x1, y1, x2, y2, x3, y3);
+  // if (area === 0) return null; // degenerate
+
+  // barycentric weights (using edge functions)
+  // const u = edge(x2, y2, x3, y3, px, py) / area; // weight for v1
+  // const v = edge(x3, y3, x1, y1, px, py) / area; // weight for v2
+  // const w = 1.0 - u - v;   
+  
   // get the color at p
-  return [r, g, b]  = [u*v1c + v*v1c + w*v1c, u*v2c + v*v2c + w*v2c, u*v3c + v*v3c + w*v3c];
+  const r = u * r1 + v * r2 + w * r3;
+  const g = u * g1 + v * g2 + w * g3;
+  const b = u * b1 + v * b2 + w * b3;
+  return [r, g, b]
 }
 
 
@@ -130,7 +138,7 @@ function pointIsInsideTriangle(v1,v2,v3,p){
   let a = y2 - y1;
   let b = x2 - x1;
   let c = x2*y1 - x1*y2;
-  if (px*a + py*b + c > 0) {
+  if (px*a + py*b + c >= 0) {
     isInside_v1v2 = true;
   } // top-left rule
   else if (px*a + py*b + c === 0) {
@@ -142,7 +150,7 @@ function pointIsInsideTriangle(v1,v2,v3,p){
   a = y3 - y2;
   b = x3 - x2;
   c = x3*y2 - x2*y3;
-  if (px*a + py*b + c > 0) {
+  if (px*a + py*b + c >= 0) {
     isInside_v2v3 = true;
   } // top-left rule
   else if (px*a + py*b + c === 0) {
@@ -151,19 +159,20 @@ function pointIsInsideTriangle(v1,v2,v3,p){
     }
   }
   // line between v1 and v3
-  a = y3 - y1;
+  a = y1 - y3;
   b = x3 - x1;
   c = x3*y1 - x1*y3;
-  if (px*a + py*b + c > 0) {
+  if (px*a + py*b + c >= 0) {
     isInside_v1v3 = true;
   } // top-left rule
   else if (px*a + py*b + c === 0) {
-    if (isTopLeft(v1,v3)) {
+    if (isTopLeft(v3,v1)) {
       isInside_v1v3 = true;
     }
   }
   return isInside_v1v2 && isInside_v2v3 && isInside_v1v3;
 }
+
 
 function isTopLeft (v0,v1) {
   const [x1, y1] = v0;
@@ -178,7 +187,7 @@ function isTopLeft (v0,v1) {
   return is_left_edge || is_top_edge;
 }
 
- const edge = (x0, y0, x1, y1, px, py) => (px - x0)*(y1 - y0) - (py - y0)*(x1 - x0);
+const edge = (x0, y0, x1, y1, px, py) => (px - x0)*(y1 - y0) - (py - y0)*(x1 - x0);
 
 // DO NOT CHANGE ANYTHING BELOW HERE
 export { Rasterizer, Framebuffer, DEF_INPUT};
